9b075fa97bd6669e5a173f5402328e923e46e7c4
Karate-do-sv <83379002+Karate-do-sv@users.noreply.github.com>
2021-05-16 00:57:41 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+document.querySelector(".menu-btn").addEventListener("click", () => {
+    document.querySelector(".nav-menu").classList.toggle("show");
+  });
+
+  ScrollReveal().reveal('.SHOWCASE');
+  ScrollReveal().reveal('.new-cards', { delay: 500 });
+  ScrollReveal().reveal('.cards-baner-one', { delay: 500 });
+  ScrollReveal().reveal('.cards-banner-two', { delay: 500 });
